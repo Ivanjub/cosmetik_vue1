9a1071623e2491f60dd8d8b255512bb46cf31486
unknown <ivanalgual@gmail.com>
2026-01-15 23:15:49 -0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
-    <h1>Bienvenido a la Tienda</h1>
+    <section class="section1">
+    <h1>Bienvenido a mi tienda</h1>
+    <a href="index.html">
+    <img class="logo" src="./assets/boutique.jpg" alt="Logo"></a>
+    </section>
+
     <CurrentView></CurrentView>
     
-
   </div>
 </template>
 
@@ -21,4 +25,28 @@ export default {
 
 <style>
 /* Aquí puedes agregar estilos globales */
+.section1 {
+  background-color: #f5f5f5;
+  width: 100%;
+  height: 150px;
+  padding: 0px;
+  margin-bottom: 20px;
+  border-radius: 8px;
+}
+h1 {
+  text-align: center;
+  position: relative;
+  top: 40px;
+  color: #34495e;
+}
+.logo {  
+  position: relative;
+  bottom: 30px;
+  left: 90px;
+  width: 150px;
+  height: auto;
+  display: left;
+  margin: 0 auto;
+}
+
 </style>
